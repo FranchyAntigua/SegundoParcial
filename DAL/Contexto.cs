ce6814b42fd492608885004b3d860c6c8a61f42e
FranchyAntigua <Franchy_antigua@ucne.edu.do>
2019-06-25 11:52:52 -0400

    Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using SegundoParcial.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Asignaturas> Asignaturas { get; set; }
+        public DbSet<Estudiante> Estudiante { get; set; }
+        public DbSet<Inscripcion> Inscripcion { get; set; }
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
